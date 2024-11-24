0e691d4aaaf5c7cf6a11b4c5131a6c86919cb65c
zo4yz <140940249+zo4yz@users.noreply.github.com>
2024-11-24 18:44:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const slider = document.querySelector('.slider');
+const list = document.querySelector('.list');
+const thumbnail = document.querySelector('.thumbnail');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
+next.addEventListener('click',() => {
+    initSlider('next');
+});
+
+prev.addEventListener('click',() => {
+    initSlider('prev');
+});
+
+const initSlider = (type) => {
+    const sliderItems = list.querySelectorAll('.item');
+    const thumbnailItems = thumbnail.querySelectorAll('.item');
+
+    if (type === 'next') {
+        list.appendChild(sliderItems[0]);
+        thumbnail.appendChild(thumbnailItems[0]);
+        slider.classList.add('next');
+    } else {
+        const lastItemPosition = sliderItems.length - 1;
+        list.prepend(sliderItems[lastItemPosition]);
+        thumbnail.prepend(thumbnailItems[lastItemPosition]);
+        slider.classList.add('prev')
+    }
+
+    setTimeout (() => {
+        slider.classList.remove('next');
+        slider.classList.remove('prev');
+    }, 2000);
+};
